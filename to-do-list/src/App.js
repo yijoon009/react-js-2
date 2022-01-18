fac60958053256876e3938d76af194500c9d6823
yjkim <yjkim@sweetk.co.kr>
2022-01-18 13:39:55 +0900

    todo 개수 추가

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -16,7 +16,7 @@ function App() {
   };
   return (
     <div>
-      <h1>My Todos</h1>
+      <h1>My Todos : ({toDos.length}) </h1>
       <form onSubmit={onSubmit}>
         <input
           value={toDo}
