0954ff3ae397f6509e16e2c6bc0e0a9681a40bb4
yjkim <yjkim@sweetk.co.kr>
2022-01-18 13:29:03 +0900

    to do list 완료

---
+++ to-do-list/src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  //입력창에 입력할때
+  const onChange = (e) => setToDo(e.target.value);
+  //제출버튼 눌렀을때
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
+  };
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your Todos"
+        ></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
