2b95d770e6a586a25bd0d1776117cda6f9254358
yjkim <yijoon009@gmail.com>
2022-01-19 09:30:16 +0900

    movie-app 최종완성

--- movie-app/src/routes/Detail.js
+++ movie-app/src/routes/Detail.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import MovieDetail from '../components/MovieDetail';
 
@@ -6,28 +6,26 @@ function Detail() {
   const [loading, setLoading] = useState(true);
   const [movieD, setMovieD] = useState([]);
   const { id } = useParams();
-  const getMovie = async () => {
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     setMovieD(json.data.movie);
     setLoading(false);
-  };
+  }, [id]);
   useEffect(() => {
     getMovie();
-  }, []);
-
+  }, [getMovie]);
+  // console.log(movieD);
   return (
     <div>
       {loading ? (
         <h2>Movie Detail Loading...</h2>
       ) : (
         <div>
-          <ul>
-            {movieD.map((item, index) => (
-              <li key={index}>{item.title}</li>
-            ))}
-          </ul>
+          <img src={movieD.medium_cover_image} alt={movieD.title} />
+          <h2>{movieD.title}</h2>
+          <p>{movieD.language}</p>
         </div>
       )}
     </div>
